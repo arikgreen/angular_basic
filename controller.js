8bccac5487a7ffd650cfb67aca1790abcf968d5f
Artur <arikgreenpl@gmail.com>
2017-10-23 23:42:57 +0200

    Add more controllers and display more controller fields.

--- controller.js
+++ controller.js
@@ -1,8 +1,11 @@
-angular.module('AngularExample').controller('exampleController', function ($scope){
+angular.module('AngularExample').controller('listController', function ($scope){
 	$scope.message = 'Hello world!';
 	$scope.list = [
 		{firstName: 'John', lastName: 'Doe'},
 		{firstName: 'Mark', lastName: 'Smith'},
 		{firstName: 'James', lastName: 'Mole' }
 	];
+});
+angular.module('AngularExample').controller('exampleController', function ($scope){
+	$scope.message = 'My first project in AngularJS!';
 });
